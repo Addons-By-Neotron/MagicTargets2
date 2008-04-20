2bbfa90c69966279dd2a1c4b7da99afba03b1f3e
neotron <neotron@36650db8-a78a-4bd3-9d7e-5b5973ed635e>
2008-04-20 22:43:48 +0000

    MagicTargets: - Add icons when a mob is crowd controlled usign a known method. - Learn new mobs from combat events (i.e don't have to target them).

--- tmp/trunk/MagicTargets/MagicTargets.lua
+++ tmp/trunk/MagicTargets/MagicTargets.lua
@@ -36,32 +36,57 @@ local UnitIsPlayer = UnitIsPlayer
 local UnitGUID = UnitGUID
 local GetRaidTargetIndex = GetRaidTargetIndex
 local fmt = string.format
+local tinsert = table.insert
+local tremove = table.remove
+
+local ccspells
 local db
 local playerInCombat = false
+ ccstrings = {}
+ mobspells = {}
 local died = {}
 local seen = {}
-
 local mmtargets = {}
 local raidicons = {}
+local ingroup   = {}
+local trivial   = {}
 local bars 
+
+local tableStore = {}
 
 local colors = {
     tank = { [0] = 0, [1] = 0.8, [2] = 0.3 },
     cc   = { [0] = 0, [1] = 0.7, [2] = 0.9 }
 }
    
-   
+
+local function get()
+   return tremove(tableStore) or {}
+end
+
+local function del(tbl)
+   if type(tbl) ~= "table" then return end
+   for id,data in pairs(tbl) do
+      if type(data) == "table" then
+	 del(data)
+      end
+      tbl[id] = nil
+   end
+   tinsert(tableStore, tbl)
+end
 
 local iconPath = "Interface\\AddOns\\MagicTargets\\Textures\\%d.tga"
 
 function mod:OnInitialize()
-   MagicTargetsDB = MagicTargetsDB or {}
+   MagicTargetsDB = MagicTargetsDB or get()
    db = MagicTargetsDB
    for i = 1,8 do
       raidicons[i] = iconPath:format(i)
    end
+   ccspells = comm.spellIdToCCID
 end
 
+-- Sort first by Magic Marker priority, then by health and lastly by guid.
 local function BarSortFunc(a, b)
    local amm = mmtargets[a.name]
    local bmm = mmtargets[b.name]
@@ -95,13 +120,33 @@ function mod:OnEnable()
    if InCombatLockdown() then
       self:PLAYER_REGEN_DISABLED()
    end
+   self:RegisterEvent("RAID_ROSTER_UPDATE", "ScheduleGroupScan")
+   self:RegisterEvent("PARTY_MEMBERS_CHANGED", "ScheduleGroupScan")
+   self:ScheduleGroupScan()
 end
 
 function mod:OnDisable()
    comm:UnregisterListener(self, "MM")
    self:UnregisterEvent("PLAYER_REGEN_ENABLED")
    self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+   self:UnregisterEvent("RAID_ROSTER_UPDATE")
+   self:UnregisterEvent("PARTY_MEMBERS_CHANGED")
    self:PLAYER_REGEN_ENABLED()
+end
+
+do
+   local groupScanTimer
+   function mod:ScheduleGroupScan()
+      if groupScanTimer then self:CancelTimer(groupScanTimer, true) end
+      groupScanTimer = self:ScheduleTimer("ScanGroupMembers", 5)
+   end
+
+   function mod:ScanGroupMembers()
+      for id in pairs(ingroup) do
+	 ingroup[id] = nil
+      end
+      mod:IterateRaid(function(self, unitname) ingroup[unitname] = true end)
+   end
 end
 
 local updated = {}
@@ -117,15 +162,23 @@ local function SetBarColor(bar,cc)
 end
 
 function mod:UpdateBar(target)
+   if not UnitExists(target) then return end
    local guid = UnitGUID(target)
    if updated[guid] then
       updated[guid] = updated[guid] + 1
       return
+   elseif trivial[guid] or died[guid] then
+      return
    end
    local type = UnitCreatureType(target)
-   if UnitExists(target) and UnitCanAttack("player", target)
-      and type ~= "Critter" and type ~= "Totem" and not UnitIsDead(target)
+   if UnitCanAttack("player", target) and not UnitIsDead(target)
       and not UnitPlayerControlled(target)  and not UnitIsPlayer(target) then
+      if type == "Critter" or type == "Totem"  then
+	 trivial[guid] = true
+	 bars:RemoveBar(guid)
+	 return
+      end
+      
       local bar = currentbars[guid]
       local mark = GetRaidTargetIndex(target)
       if not bar then
@@ -148,31 +201,48 @@ function mod:UpdateBar(target)
 end
 
 function mod:UpdateBars()
-   currentbars = bars:GetBars() or {}
+   currentbars = bars:GetBars() or get()
    for id in pairs(updated) do updated[id] = nil end
-
+   
    for id,data in pairs(mmtargets) do
-      local bar = currentbars[id]
-      if not bar then
-	 local bar = bars:NewCounterBar(id, data.cc and fmt("%s (%s)", data.name, data.cc) or data.name, 100, 100, GetRaidIcon(data.mark))
-	 bar.mark = data.mark
-      else
-	 if bar.mark ~= data.mark then
-	    bar:SetIcon(GetRaidIcon(data.mark))
+      if not died[id] then
+	 local bar = currentbars[id]
+	 if not bar then
+	    local bar = bars:NewCounterBar(id, data.cc and fmt("%s (%s)", data.name, data.cc) or data.name, 100, 100, GetRaidIcon(data.mark))
 	    bar.mark = data.mark
+	 else
+	    if bar.mark ~= data.mark then
+	       bar:SetIcon(GetRaidIcon(data.mark))
+	       bar.mark = data.mark
+	    end
+	    bar.label:SetText(data.cc and fmt("%s (%s)", data.name, data.cc) or data.name)
 	 end
-	 bar.label:SetText(data.cc and fmt("%s (%s)", data.name, data.cc) or data.name)
       end
    end
    if playerInCombat then
-      mod:IterateTargets(self.UpdateBar)
+      mod:IterateRaid(self.UpdateBar, true)
    end
-   if next(currentbars) then
-      for id in pairs(currentbars) do
-	 if  updated[id] or mmtargets[id] or seen[id] then
-	    currentbars[id].timerLabel:SetText(updated[id] and tostring(updated[id]) or "")
-	 else
-	    bars:RemoveBar(id)	
+   currentbars = bars:GetBars()
+   if currentbars then
+      for id,name in pairs(seen) do
+	 if not currentbars[id] and not trivial[id] and not died[id] then
+	    currentbars[id] = bars:NewCounterBar(id, name, 100, 100)
+	 end
+      end
+      if next(currentbars) then
+	 for id in pairs(currentbars) do
+	    if  updated[id] or mmtargets[id] or seen[id] then
+	       if mobspells[id] and not ccstrings[id] then
+		  local str = " "
+		  for _,tex in pairs(mobspells[id]) do
+		     str = fmt("%s|T%s:0|t", str, tex)
+		  end
+		  ccstrings[id] = str
+	       end
+	       currentbars[id].timerLabel:SetText(fmt("%s%s", updated[id] and tostring(updated[id]) or "", ccstrings[id] or ""))
+	    else
+	       bars:RemoveBar(id)	
+	    end
 	 end
       end
    end
@@ -181,23 +251,33 @@ end
 
 do
    local raidtarget, partytarget
-   function mod:IterateTargets(callback, ...)
-      local id, name, class
+   function mod:IterateRaid(callback, target, ...)
+      local id, name, class, map
       if GetNumRaidMembers() > 0 then
-	 if not raidtarget then raidtarget = {} end
+	 if target then 
+	    if not raidtarget then raidtarget = get() end
+	    map = raidtarget
+	 end
 	 for id = 1,GetNumRaidMembers() do
-	    if not raidtarget[id] then raidtarget[id] = "raid"..id.."target" end
-	    callback(self, raidtarget[id], ...)
+	    if target then
+	       if not map[id] then map[id] = "raid"..id..(target and "target" or "") end
+	       callback(self, map[id], ...)
+	    else
+	       callback(self, GetRaidRosterInfo(id), ...)
+	    end
 	 end
       else
 	 if GetNumPartyMembers() > 0 then
-	    if not partytarget then partytarget = {} end
+	    if target then 
+	       if not partytarget then partytarget = get() end
+	       map = partytarget
+	    end
 	    for id = 1,GetNumPartyMembers() do
-	       if not partytarget[id] then partytarget[id] = "party"..id.."target" end
-	       callback(self, partytarget[id], ...)
+	       if not map[id] then map[id] = "party"..id end
+	       callback(self, target and (map[id].."target") or UnitName(map[id]), ...)
 	    end
 	 end
-	 callback(self, "target", ...);
+	 callback(self, target and "target" or UnitName("player"), ...);
       end   
    end
 end
@@ -221,17 +301,17 @@ end
 
 function mod:OnCommMarkV2(mark, guid, _, name)
    if not name then return end
-   if mmtargets[guid] then
-      mmtargets[guid].name = name
-      mmtargets[guid].mark = mark
-   else
-      mmtargets[guid] = { name = name, mark = mark }
+   if not mmtargets[guid] then
+      mmtargets[guid] = get()
    end
+   mmtargets[guid].name = name
+   mmtargets[guid].mark = mark
    self:UpdateBars()
 end
 
 function mod:OnCommUnmarkV2(guid, mark)
    if mmtargets[guid] then
+      del(mmtargets[guid])
       mmtargets[guid] = nil
    end
    self:UpdateBars()
@@ -258,12 +338,14 @@ function mod:PLAYER_REGEN_ENABLED()
 	 end
       end
    end
-   for id in pairs(died) do
-      died[id] = nil
+   for id in pairs(died) do died[id] = nil end
+   for id in pairs(ccstrings) do ccstrings[id] = nil end
+   for id,data in pairs(mobspells) do
+      del(data)
+      mobspells[id] = nil
    end
-   for id in pairs(seen) do
-      seen[id] = nil
-   end
+   for id in pairs(seen) do seen[id] = nil end
+   for id in pairs(trivial) do trivial[id] = nil end
    playerInCombat = false
 end
 
@@ -276,18 +358,72 @@ function mod:PLAYER_REGEN_DISABLED()
    playerInCombat = true
 end
 
-function mod:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, _, _, _,
-					 guid, name, _, spellid, spellname)
-   if not seen[guid] then
-      -- yes this is a it ugly but we keep track of mobs we've seen combat events from so we can show them
-      seen[guid] = true
+local bit_band = bit.band
+local sub = string.sub
+
+local function GetFlagInfo(flags)
+   return
+   bit_band(flags, COMBATLOG_OBJECT_AFFILIATION_MINE+COMBATLOG_OBJECT_AFFILIATION_PARTY+COMBATLOG_OBJECT_AFFILIATION_RAID)~=0, -- in group
+   bit_band(flags, COMBATLOG_OBJECT_TYPE_PLAYER)==COMBATLOG_OBJECT_TYPE_PLAYER, -- is player
+   bit_band(flags, COMBATLOG_OBJECT_REACTION_FRIENDLY) ~= 0 -- is friendly
+end
+
+function mod:COMBAT_LOG_EVENT_UNFILTERED(_, _, event, sguid, sname, sflags,
+					 tguid, tname, tflags, spellid, spellname)
+   local sinGroup, sisPlayer, sisFriend = GetFlagInfo(sflags)
+   local tinGroup, tisPlayer, tisFriend = GetFlagInfo(tflags)
+
+   local sispet = bit_band(sflags, COMBATLOG_OBJECT_TYPE_PET+COMBATLOG_OBJECT_TYPE_GUARDIAN)~=0
+   local sisnpc = bit_band(sflags, COMBATLOG_OBJECT_TYPE_NPC) == COMBATLOG_OBJECT_TYPE_NPC
+
+   local tispet = bit_band(tflags, COMBATLOG_OBJECT_TYPE_PET+COMBATLOG_OBJECT_TYPE_GUARDIAN)~=0
+   local tisnpc = bit_band(tflags, COMBATLOG_OBJECT_TYPE_NPC) == COMBATLOG_OBJECT_TYPE_NPC
+
+   local sisfriend = (sinGroup and not sispet) or (sisPlayer and sisFriend)
+   local tisfriend = (tinGroup and not tispet) or (tisPlayer and tisFriend)
+
+
+   -- This stuff here is meant to detect new mobs we're engaged with.
+   if sisfriend and (tisnpc and not tispet) then
+      if tguid and tname and not seen[tguid] and sname and ingroup[sname] then
+--	 mod:trace("Recording %s as seen.", tname)
+	 seen[tguid] = tname
+      end
+   elseif tisfriend and (sisnpc and not sispet) then
+      if sguid and sname and not seen[sguid] and tname and ingroup[tname] then
+--	 mod:trace("Recording %s as seen.", sname)
+	 seen[sguid] = sname
+      end
    end
    
+   if seen[tguid] or mmtargets[tguid] and not died[tguid] then
+      -- This is on ze list of known npcs, and it's alive
+      if event == "UNIT_DIED" or event == "PARTY_KILL" or event == "UNIT_DESTROYED" then
+	 died[tguid] = true
+	 bars:RemoveBar(tguid)
+      end
+   end
    if event == "SPELL_AURA_APPLIED" then
-      -- mark CC'd mobs
-   elseif event == "UNIT_DIED" or event == "PARTY_KILL" or event == "UNIT_DESTROYED" then
-      died[guid] = true
-      seen[guid] = nil
-      mmtargets[guid] = nil
+      -- record crowd control
+      if ccspells[spellid] then
+	 local cc = mobspells[tguid] or get()
+	 if not cc[spellid] then 
+	    cc[spellid] = select(3, GetSpellInfo(spellid))
+	    mobspells[tguid] = cc
+	    ccstrings[tguid] = nil
+	 end
+      end
+   elseif event == "SPELL_AURA_REMOVED" then
+      local cc = mobspells[tguid]
+      if cc then
+	 if cc[spellid] then
+	    cc[spellid] = nil
+	    ccstrings[tguid] = nil
+	    if not next(cc) then
+	       del(cc)
+	       mobspells[tguid] = nil
+	    end
+	 end
+      end
    end
 end
